f5c579e0001b5c8b1c759f138543d5c69e5a0040
Yars2021 <yaroslav.sukhovey@yandex.ru>
2021-11-08 22:31:55 +0300

    Added int_list (prototype for list)

--- main.c
+++ main.c
@@ -1,9 +1,16 @@
 #include <stdio.h>
 #include "st_string.h"
+#include "int_list.h"
 
 int main() {
     st_string* string = st_create("Hello");
-    printf("%s", string->chars);
+    printf("%s\n\n", string->chars);
     st_destroy(string);
+
+    int_list* list = int_list_create();
+    int_list_append(list, 123);
+    int_list_prepend(list, 987);
+    printf("%d", int_list_get_item(list, 1));
+    int_list_destroy(list);
     return 0;
 }
